33bbff7babaa35720e462f0a6ef35c93edb63705
SomonSikder <somonsikder777@gmail.com>
2022-12-17 15:14:45 +0600

    Category and Features

---
+++ src/components/UI/category/Category.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../../../styles/category.css";
+import categoryImg01 from "../../../assets/images/category-01.png";
+import categoryImg02 from "../../../assets/images/category-02.png";
+import categoryImg03 from "../../../assets/images/category-03.png";
+import categoryImg04 from "../../../assets/images/category-04.png";
+
+const categoryData = [
+  {
+    display: "Fast Food",
+    imgUrl: categoryImg01,
+  },
+  {
+    display: "Pizza",
+    imgUrl: categoryImg02,
+  },
+  {
+    display: "Asian Food",
+    imgUrl: categoryImg03,
+  },
+  {
+    display: "Row Meat",
+    imgUrl: categoryImg04,
+  },
+];
+
+const Category = () => {
+  return (
+    <Container>
+      <Row>
+        {categoryData.map((item, index) => (
+          <Col lg="3" md="4">
+            <div className="category-item">
+              <div className="category-img">
+                <img src={item.imgUrl} alt="Category_item" />
+              </div>
+              <h6>{item.display}</h6>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Category;
